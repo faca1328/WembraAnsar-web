a661ec73cf598b0284d7602597e4a0faaecdd82d
Facu M <facundoivanmartinez@hotmail.com>
2024-05-11 20:18:32 -0300

    back change

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,12 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
+
 import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [preact(), tailwind(), mdx()],
-  images: {
-    service: 'noop'
-  },
-
-  // Excluir dotenv del proceso de empaquetado
-  exclude: [
-    'dotenv'
-  ]
+  integrations: [preact(), tailwind(), mdx()]
 });
-
