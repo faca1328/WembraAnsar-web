c36b97bd683c093666c331573ad47c66c84701d8
Facu M <facundoivanmartinez@hotmail.com>
2024-05-11 20:00:47 -0300

    manual ssr to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 
@@ -7,5 +8,6 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: cloudflare(),
   integrations: [preact(), tailwind(), mdx()]
 });
