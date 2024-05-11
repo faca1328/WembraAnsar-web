e9a57c0c69c27e512dc618a80c9e04bcf17ea67a
Facu M <facundoivanmartinez@hotmail.com>
2024-05-11 20:31:38 -0300

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://example.com',
   output: "server",
   adapter: cloudflare(),
   integrations: [preact(), tailwind(), mdx()]
