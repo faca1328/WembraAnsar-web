4f213a3d44d2ade8ed08c83985080a5b1fd065ab
Facu M <facundoivanmartinez@hotmail.com>
2024-05-12 13:11:04 -0300

    befor trying netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
@@ -8,7 +7,6 @@ import node from '@astrojs/node';
 
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(),
   integrations: [preact(), tailwind(), mdx()],
   adapter: node({
     mode: 'standalone',
